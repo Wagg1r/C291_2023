1bdb4447a2c1eba681ca7b342a8c98334da3e9f6
Thomas Mitchell Waggoner <thomwagg@silo.luddy.indiana.edu>
2023-01-18 16:30:18 -0500

    final submit for week 1 assignment 1

--- exercises/wk1/hello.c
+++ exercises/wk1/hello.c
@@ -1,17 +1,28 @@
 /* 
  * Basic C program to print out hello world! 
- * Author: Kurt Seiffert
+ * Author: Thomas Waggoner
 */
 
 #include <stdio.h>
+#include <unistd.h>
 
 int main(void) {
-  int myNumber = 0;
-  printf("Hello: Welcome to C291!\n");
-  printf("Please enter a number and press ENTER: ");
-  scanf("%d", &myNumber);  
-  printf("Here is the number yor entered: %d\n",myNumber);
+  int myNumber = 100;
+  int counter = 5;
+  printf("\" *****{Welcome to C291 Spring 2023 @ IU}, %%%%%% Golden Rule: Start working on everything as early as possible Thanks %%%%%% *****\" \n");
+  while (myNumber >= 0){  
+  printf("%04d", myNumber);
+  fflush(stdout);
+  while(counter >= 0) {
+	if (myNumber % 2 == 0)
+	printf(".");
+	sleep(1);
+	fflush(stdout);
+	counter = counter -1;
+	} //ends counter while loop
+    counter = 5;
+    myNumber = myNumber -1;
+    printf("\n");
+  } // ends  while loop 
   return(0); 
-}
-
-
+ } // ends main
